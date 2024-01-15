b01b469b926d08a367c79b66c18842317a33b06b
masud1989 <mranasrj@gmail.com>
2024-01-15 15:22:11 +0600

    search-ok

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -63,6 +63,13 @@ export default function TaskBoard() {
     setTasks(newTasks);
   }
 
+  function handleSearch(searchText) {
+    const filteredTasks = tasks.filter((task) =>
+      task.title.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setTasks([...filteredTasks]);
+  }
+
   return (
     <section className="mb-20" id="tasks">
       {showAddEditModal && (
@@ -74,7 +81,7 @@ export default function TaskBoard() {
       )}
       <div className="container">
         <div className="p-2 flex justify-end">
-          <SearchTask />
+          <SearchTask onSearch={handleSearch} />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions
